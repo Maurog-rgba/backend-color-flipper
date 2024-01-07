34c2737c27c19cb9c504d57af285a1f9e8342406
Mauro Gabriel dos Santos <gabriel.mauro.santos.3@gmail.com>
2024-01-06 21:47:42 -0300

    changed db and cors settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL, 
+  });
+
+  app.enableCors({
+    origin: true, 
+  });
+
   await app.listen(3000);
   console.log('Server running on http://localhost:3000');
 }
